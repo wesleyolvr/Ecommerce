3d6b8afb0f85a56ec0749d747e332b33c640732f
Wesley Oliveira <wesleyumsocaminho@gmail.com>
2018-08-23 18:50:17 -0300

    add migrations

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_URL = '/static/'
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO','https')
 
-STATIC_URL = '/static/'
 
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT=os.path.join(PROJECT_ROOT,'staticfiles')
